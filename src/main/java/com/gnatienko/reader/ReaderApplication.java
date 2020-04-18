602c3468d41696b9976592c96c1167176272ae7f
Gnatienko <gnatienko@gmail.com>
2020-04-18 17:01:45 +0300

    First commit

---
+++ src/main/java/com/gnatienko/reader/ReaderApplication.java
@@ -0,0 +1,14 @@
+package com.gnatienko.reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaderApplication.class, args);
+		System.out.println("Helo Alexey");
+	}
+
+}

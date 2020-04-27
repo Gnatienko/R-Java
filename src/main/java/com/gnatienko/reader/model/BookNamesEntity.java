a1ee5745ff85b001edfe8fa5667f3dc9f35a19e5
Gnatienko <gnatienko@gmail.com>
2020-04-27 21:18:47 +0300

    [reade_0] add user table migration

--- src/main/java/com/gnatienko/reader/model/BookNamesEntity.java
+++ src/main/java/com/gnatienko/reader/model/BookNamesEntity.java
@@ -6,6 +6,14 @@ import java.util.Objects;
 @Entity
 @Table(name = "book_names")
 public class BookNamesEntity {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Creating new IDs
     private Long id;
@@ -16,29 +24,6 @@ public class BookNamesEntity {
     @Column(name = "name")
     private String name;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long user_id) {
-        this.userId = user_id;
-    }
 
     @Override
     public boolean equals(Object o) {
